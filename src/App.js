6e499e7ccc42e0b1327439372d18278e55bd83dd
Eliud <oeliud8@gmail.com>
2022-03-15 00:37:22 -0600

    Adding updates

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';
-import Home from './components/Home';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      {/* <Header/> */}
+      <Header />
       <Home />
-
-
     </div>
   );
 }
